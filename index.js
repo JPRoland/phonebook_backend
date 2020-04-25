5f9ef58695637b290aeb7b1f46963883bb4facae
Joe Roland <joroland@gmail.com>
2020-04-25 19:58:18 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3001
+
+const persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+app.use(express.json())
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const person = persons.find(p => p.id === Number(req.params.id))
+
+    if (!person) {
+        return res.status(404).json({ error: 'Person not found' })
+    }
+
+    res.json(person)
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(`<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date.toString()}</p>
+    </div>`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
