3522dd842020dcc7a138a5736399786aa98f146f
Joe Roland <joroland@gmail.com>
2020-04-26 16:15:23 -0400

    Added frontend

---
+++ build/precache-manifest.6bde16ea23688713f23cdfac222657bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792f7df58087402dc77d9a78389a9421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5aea9e6995e86dd90e6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2b5fcf915faa7dc65c9f",
+    "url": "/static/js/2.da58ffa3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.da58ffa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5aea9e6995e86dd90e6",
+    "url": "/static/js/main.458c538b.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
--- index.js
+++ index.js
@@ -38,6 +38,8 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+app.use(express.static('build'))
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
