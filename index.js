a588f4e591b4d9f792485f870096b67dc6e25b9e
Joe Roland <joroland@gmail.com>
2020-04-28 21:00:05 -0400

    Added eslint config

--- index.js
+++ index.js
@@ -8,116 +8,98 @@ const app = express()
 
 const PORT = process.env.PORT || 3001
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === "CastError") {
-        return res.status(400).send({ error: 'Malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'Malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
-morgan.token('body', (req, res) => JSON.stringify(req.body))
+morgan.token('body', (req, _res) => JSON.stringify(req.body))
 
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-app.get('/api/persons', (req, res) => {
-    Person.find({}).then(people => {
-        res.json(people.map(person => person.toJSON()))
-    })
+app.get('/api/persons', (_req, res) => {
+  Person.find({}).then((people) => {
+    res.json(people.map((person) => person.toJSON()))
+  })
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person
+    .save()
+    .then((savedPerson) => savedPerson.toJSON())
+    .then((savedAndFormattedPerson) => {
+      res.json(savedAndFormattedPerson)
     })
-
-    person
-        .save()
-        .then(savedPerson => savedPerson.toJSON())
-        .then(savedAndFormattedPerson => {
-            res.json(savedAndFormattedPerson)
-        })
-        .catch(err => next(err))
+    .catch((err) => next(err))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id).then(person => {
-        if (!person) {
-            return res.status(404).end()
-        }
-        res.json(person.toJSON())
-    }).catch(err => {
-        next(err)
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (!person) {
+        return res.status(404).end()
+      }
+      res.json(person.toJSON())
+    })
+    .catch((err) => {
+      next(err)
     })
 })
 
 app.put('/api/persons/:id', async (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson.toJSON())
-        })
-        .catch(err => next(err))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch((err) => next(err))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id).then(result => {
-        res.status(204).end()
-    }).catch(err => next(err))
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch((err) => next(err))
 })
 
 app.get('/info', (req, res) => {
-    const date = new Date()
-    Person.countDocuments()
-        .then(count => {
-            res.send(`<div>
+  const date = new Date()
+  Person.countDocuments().then((count) => {
+    res.send(`<div>
                         <p>Phonebook has info for ${count} people</p>
                         <p>${date.toString()}</p>
                       </div>`)
-        })
+  })
 })
 
 app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -5,27 +5,32 @@ const url = process.env.MONGODB_URI
 
 console.log(`Connecting to ${url}`)
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .then(result => {
-        console.log('Connected to MongoDB')
-    })
-    .catch(err => {
-        console.log(`Error connecting to MongoDB: ${err.message}`)
-    })
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((_result) => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((err) => {
+    console.log(`Error connecting to MongoDB: ${err.message}`)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true, minlength: 3 },
-    number: { type: String, required: true, minlength: 8 }
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, minlength: 8 },
 })
 
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
 })
 
 module.exports = mongoose.model('Person', personSchema)
